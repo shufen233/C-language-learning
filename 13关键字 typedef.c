7f2b89f93f59e3b1fcbf1a6d310323e48e1dea24
树彤 宋 <2837425301@qq.com>
2023-12-20 19:36:19 +0800

    更改

---
+++ 13关键字 typedef.c
@@ -0,0 +1,12 @@
+//关键字 typedef
+//ypedef顾名思义是类型定义，这里应该理解为类型重命名。
+//比如：
+//将unsigned int重命名为uint32,所以uint32也是一个类型名
+//typedef unsigned int uint_32;
+//int main()
+//{
+//	//观察num1和num2,这两个变量的类型是一样的
+//	unsigned int num1 0;
+//	uint_32 num2 0;
+//	return 0;
+//}
