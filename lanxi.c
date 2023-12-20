3448052ad4a7faafcc68f295b728e82c62e59467
28374 <28374@009>
2023-12-20 16:55:22 +0800

    添加项目文件。

---
+++ lanxi.c
@@ -0,0 +1,12 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d,", &a,&b);
+//	printf("%d %d\n", b,a);
+//
+//	return 0;
+//}
