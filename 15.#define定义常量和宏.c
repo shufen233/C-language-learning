a1cf956667ac7bceacfe613bb6e32ab6f29ba6b3
shufen233 <2837425301@qq.com>
2023-12-20 23:09:11 +0800

    2023.12.20

---
+++ 15.#define定义常量和宏.c
@@ -0,0 +1,21 @@
+// #define定义常量和宏
+//define定义标识符常量
+//#define NUM 1000
+//int main()
+//{
+//	printf("%d\n", NUM);
+//	return 0;
+//}
+
+//define定义宏
+//#define ADD(x,y)((x)+(y))
+//宏的格式:#define "宏名" (宏参数) (宏体)
+//int main()
+//{
+//	int sum = ADD(2, 3);
+//	printf("sum=%d\n",sum);
+//
+//	sum = 10 * ADD(2, 3);
+//	printf("sum=%d\n",sum);
+//	return 0;
+//}
--- lanxi.c
+++ lanxi.c
@@ -1,12 +1,16 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <math.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
 //int main()
 //{
-//	int a = 0;
-//	int b = 0;
-//	scanf("%d%d,", &a,&b);
-//	printf("%d %d\n", b,a);
+//	int a = 10;
+//	printf("%p\n",&a);// &a; // 取内存地址操作符
+//	int* p = &a;
+//	//int说明p指向的对象是int类型的
+//	// *说明p是指针变量
+//	//p就是指针变量
 //
+//	char ch = 'w';
+//	char* pc = &ch;
 //	return 0;
 //}
