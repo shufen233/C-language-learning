73d6f481a2c16b27c1e6b350461d77655e0b0b21
树彤 宋 <2837425301@qq.com>
2023-12-20 17:20:59 +0800

    shufen

---
+++ 数据类型.c
@@ -0,0 +1,22 @@
+//数据类型
+//char        //字符数据类型
+//short       //短整型
+//int         //整形
+//long        //长整形
+//long long   //更长整形
+//float       //单精度浮点数
+//double      //双精度浮点型
+
+//不同类型的大小
+//int main()
+//{
+//	printf("%d\n",(int)sizeof(char));//因为sizof()的值的类型不是int，使用(int)强制转换成interesting型
+//	printf("%d\n",(int)sizeof(short));
+//	printf("%d\n",(int)sizeof(int));
+//	printf("%d\n",(int)sizeof(long));
+//	printf("%d\n",(int)sizeof(long long));
+//	printf("%d\n",(int)sizeof(float));
+//	printf("%d\n",(int)sizeof(double));
+//	return 0;
+//}
+//sizeof()函数是求字节数函数
